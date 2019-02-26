eee27877080c8f888cecd96836fccdb543381a86
Liam Sullivan <30022060@student.toiohomai.ac.nz>
2019-02-27 10:47:29 +1300

    week1 prog

---
+++ WeekOneConsoleApp/WeekOneConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeekOneConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            string name;
+            Console.WriteLine(" Enter your age.");
+            age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter your name");
+            name = Console.ReadLine();
+
+
+            Console.WriteLine(name + age);
+            Console.ReadLine();
+            
+
+        }
+    }
+}
